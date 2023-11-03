e7bc5a27b9c41fefbea34505b7e3e896e03a3362
TANMAY <tanmaymvaidya@gmail.com>
2023-11-03 18:38:03 +0530

    print Even or Odd number using array

---
+++ src/main/java/com/bootcoding/array/Array4.java
@@ -0,0 +1,16 @@
+package com.bootcoding.array;
+public class Array4 {
+    public static void main(String[] args) {
+        int arr[]={4,43,6,2,35,3,9,8,45,9,32};
+        for(int i=0;i<11;i++)
+        {
+            if(arr[i]%2==0)
+            {
+                System.out.println("These is even Number:"+arr[i]);
+            }
+            else{
+                System.out.println("These is odd Number:"+arr[i]);
+            }
+        }
+    }
+}
