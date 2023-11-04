2bda52b8bf4f462944d57dcd99a18cba21699d8a
TANMAY <tanmaymvaidya@gmail.com>
2023-11-04 18:40:06 +0530

    print table using array

---
+++ src/main/java/com/bootcoding/array/Array7.java
@@ -0,0 +1,12 @@
+package com.bootcoding.array;
+public class Array7 {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5,6,7,8,9,10};
+        int p;
+        for(int i=0;i<arr.length;i++)
+        {
+         p=5*arr[i];
+            System.out.println(5+"*"+arr[i]+"="+p);
+        }
+    }
+}
