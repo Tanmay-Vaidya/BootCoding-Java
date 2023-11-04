a963d5dd6ee305bbd6f7f3ebf7c360a11f3046be
TANMAY <tanmaymvaidya@gmail.com>
2023-11-04 16:48:37 +0530

    Array example program using length

---
+++ src/main/java/com/bootcoding/array/Example3.java
@@ -0,0 +1,11 @@
+package com.bootcoding.array;
+public class Example3 {
+    public static void main(String[] args) {
+        int arr[]=new int[6];
+        for (int i=0;i< arr.length;i++)
+        {
+            arr[i]=i*2;
+            System.out.println(arr[i]);
+        }
+    }
+}
