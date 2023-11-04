f25b32dccde914ef928f70443e80282e12bccc65
TANMAY <tanmaymvaidya@gmail.com>
2023-11-04 16:47:50 +0530

    Array example program using increment

---
+++ src/main/java/com/bootcoding/array/Example2.java
@@ -0,0 +1,12 @@
+package com.bootcoding.array;
+
+public class Example2 {
+    public static void main(String[] args) {
+        int arr[]=new int[5];
+        for(int i=0;i<5;i++)
+        {
+            arr[i]=i*2;
+            System.out.println(arr[i]);
+        }
+    }
+}
