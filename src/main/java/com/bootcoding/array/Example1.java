f4de53e5d583ad07c37e8605709d05dc369db927
TANMAY <tanmaymvaidya@gmail.com>
2023-11-04 16:47:14 +0530

    Array example program

---
+++ src/main/java/com/bootcoding/array/Example1.java
@@ -0,0 +1,10 @@
+package com.bootcoding.array;
+public class Example1 {
+    public static void main(String[] args) {
+        int arr[]=new int[5];
+        for(int i=0;i<5;i++)
+        {
+            System.out.println(arr[i]);
+        }
+    }
+}
