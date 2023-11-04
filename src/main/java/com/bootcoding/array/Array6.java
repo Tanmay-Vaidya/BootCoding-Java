430a2651272534d5ab7b2f4ba4f223645c1dbb21
TANMAY <tanmaymvaidya@gmail.com>
2023-11-04 18:20:34 +0530

    print program using length

---
+++ src/main/java/com/bootcoding/array/Array6.java
@@ -0,0 +1,12 @@
+package com.bootcoding.array;
+public class Array6 {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+        int count;
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.println(arr[i]);
+        }
+
+    }
+}
