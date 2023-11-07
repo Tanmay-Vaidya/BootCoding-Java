136d4b7e712bf7d48ce75cd1332685d225ec6726
TANMAY <tanmaymvaidya@gmail.com>
2023-11-07 18:23:37 +0530

    Write a program to print given all elrment Multiply using array

---
+++ src/main/java/com/bootcoding/array/Array11.java
@@ -0,0 +1,13 @@
+package com.bootcoding.array;
+
+public class Array11 {
+    public static void main(String[] args) {
+        int arr[]={5,8,4,7,2,5,7};
+        int Multi=1;
+        for(int i=0;i< arr.length;i++)
+        {
+            Multi=arr[i]*Multi;
+        }
+        System.out.println(Multi);
+    }
+}
