91570b9647a474ac9217feba1fc4fe61e2541b87
TANMAY <tanmaymvaidya@gmail.com>
2023-11-07 17:58:43 +0530

    Write a program to print Sum of all given element using array

---
+++ src/main/java/com/bootcoding/array/Add.java
@@ -0,0 +1,12 @@
+package com.bootcoding.array;
+public class Add {
+    public static void main(String[]args){
+        int arr[]={4,6,9,20};
+        int Sum=0;
+        for(int i=0;i<4;i++)
+        {
+            Sum=arr[i]+Sum;
+        }
+        System.out.println(Sum);
+    }
+}
