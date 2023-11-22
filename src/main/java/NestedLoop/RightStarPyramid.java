2990f774051f7e4187fe76eed5c8f29672c1edd6
Tanmay <tanmaymvaidya@gmail.com>
2023-11-22 19:05:01 +0530

    print right half pyramid

---
+++ src/main/java/NestedLoop/RightStarPyramid.java
@@ -0,0 +1,17 @@
+package NestedLoop;
+
+public class RightStarPyramid {
+    public static void main(String[] args) {
+
+        for(int i=0;i<=3;i++)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
--- src/main/java/com/bootcoding/array/ContainsElement.java
+++ src/main/java/com/bootcoding/array/ContainsElement.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class ContainsElement {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the array value:");
+        System.out.print("Enter the array value:");
         int n=sc.nextInt();
         int arr[]={22,13,46,34,67};
         for (int i=0;i<arr.length;i++)
