ae0294f48759df62eb1f0d347fdbf0b9b97f3c46
Tanmay <tanmaymvaidya@gmail.com>
2023-11-25 11:59:15 +0530

    Write a program to check whether entered number is positive or Negative

--- src/main/java/LogicalProgram/PositiveNegativeNumber.java
+++ src/main/java/LogicalProgram/PositiveNegativeNumber.java
@@ -2,15 +2,13 @@ package LogicalProgram;
 import java.util.Scanner;
 public class PositiveNegativeNumber {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.print("Enter a Number:");
-        int a=sc.nextInt();
-        int i=0;
-        if (i<a)
-        {
+        int a = sc.nextInt();
+        int i = 0;
+        if (i < a) {
             System.out.println("These is positive Number:");
-        }
-        else {
+        } else {
             System.out.println("These is Negative Number:");
         }
     }
