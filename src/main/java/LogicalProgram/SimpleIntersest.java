c5bcf7da2f00c708c3ed15994d2afac5b4fac775
Tanmay <tanmaymvaidya@gmail.com>
2023-11-25 11:36:15 +0530

    Write a program to find Simple Interset

---
+++ src/main/java/LogicalProgram/SimpleIntersest.java
@@ -0,0 +1,13 @@
+package LogicalProgram;
+import java.util.Scanner;
+public class SimpleIntersest {
+    public static void main(String[] args) {
+
+        float Si;
+        float p=1200;
+        float r=12;
+        float t=2;
+        Si=(p*r*t)/100;
+        System.out.println("The Simple Intersest is:"+Si);
+    }
+}
