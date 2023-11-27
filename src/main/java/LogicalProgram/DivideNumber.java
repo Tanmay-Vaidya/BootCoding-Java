957db19d531decc1211b9945dc6745c9d166b575
Tanmay <tanmaymvaidya@gmail.com>
2023-11-27 11:59:20 +0530

    Find the Number upto 100 which is divisible by 3

---
+++ src/main/java/LogicalProgram/DivideNumber.java
@@ -0,0 +1,15 @@
+package LogicalProgram;
+
+public class DivideNumber {
+    public static void main(String[] args) {
+        int a=100;
+        for(int i=1;i<=a;i++)
+        {
+            if(i%3==0)
+            {
+                System.out.println("These is divisible by:"+i);
+            }
+
+        }
+    }
+}
