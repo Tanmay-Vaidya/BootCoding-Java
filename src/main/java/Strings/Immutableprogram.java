070b116001ccbca0d50394b92cf0f307a3e9ce2a
Tanmay <tanmaymvaidya@gmail.com>
2023-11-29 17:01:15 +0530

    program to print string immutable

---
+++ src/main/java/Strings/Immutableprogram.java
@@ -0,0 +1,11 @@
+package Strings;
+
+import java.sql.SQLOutput;
+
+public class Immutableprogram {
+    public static void main(String[] args) {
+        String a=("Tanmay");
+        System.out.println(a);
+
+    }
+}
