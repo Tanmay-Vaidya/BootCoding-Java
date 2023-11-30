7710192484d27f3806085129935b944a0fdd1739
Tanmay <tanmaymvaidya@gmail.com>
2023-11-30 13:37:37 +0530

    program to print camparison operator using string

---
+++ src/main/java/Strings/StringOperator.java
@@ -0,0 +1,16 @@
+package Strings;
+
+public class StringOperator {
+    public static void main(String[] args) {
+        String a="Rohit";
+        String b="Rohit";
+
+        if (a==b)
+        {
+            System.out.println("True");
+        }
+        else {
+            System.out.println("False");
+        }
+    }
+}
