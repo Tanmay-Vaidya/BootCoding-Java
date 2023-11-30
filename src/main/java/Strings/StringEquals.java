b05303acefbb2fc2c2c691205d9ab603b223514f
Tanmay <tanmaymvaidya@gmail.com>
2023-11-30 13:36:48 +0530

    program to print using Equals method

---
+++ src/main/java/Strings/StringEquals.java
@@ -0,0 +1,16 @@
+package Strings;
+
+public class StringEquals {
+    public static void main(String[] args) {
+        String a=new String("Rohit");
+        String b=new String("Bhoge");
+
+        if (a.equals(b))
+        {
+            System.out.println("True");
+        }
+        else {
+            System.out.println("False");
+        }
+    }
+}
