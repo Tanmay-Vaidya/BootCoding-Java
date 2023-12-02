1ab82319f1d347ff66afbc47ef3f3c6d252d49ff
Tanmay <tanmaymvaidya@gmail.com>
2023-12-02 12:48:32 +0530

    Local Variable Program

---
+++ src/main/java/com/bootcoding/Variables/LocalVariable.java
@@ -0,0 +1,9 @@
+class LocalVariable{
+     public static void main(String []args){
+     int firstNumber=20;
+     int secondNumber=40;
+     System.out.println(firstNumber);
+     System.out.println(secondNumber);
+     }
+}
+
