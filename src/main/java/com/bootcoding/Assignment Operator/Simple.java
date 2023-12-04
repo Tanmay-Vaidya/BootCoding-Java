175b646d1cf7908de94ebafed0cf47317133b57a
Tanmay <tanmaymvaidya@gmail.com>
2023-12-04 16:26:33 +0530

    Assignment Operator program

---
+++ src/main/java/com/bootcoding/Assignment Operator/Simple.java
@@ -0,0 +1,9 @@
+class Simple{
+      public static void main(String[]args){
+      int principal=4;
+      int rate=40;
+      int time=2; 
+      int SI = principal * (1 + rate * time);
+      System.out.println(SI);
+      }
+}
---
+++ src/main/java/com/bootcoding/Assignment Operator/SimpleInterest.java
@@ -0,0 +1,9 @@
+class SimpleInterest{
+      public static void main(String[]args){
+      int principal=4;
+      int rate=40;
+      int time=2; 
+      simpleInterest = principal * (1 + rate * time);
+      System.out.println(simpleInterest);
+      }
+}
