39a5a3ef2b2311990a00c80ddb76c6a702ddc772
Tanmay <tanmaymvaidya@gmail.com>
2023-12-04 16:16:32 +0530

    Assignment operator program

---
+++ src/main/java/com/bootcoding/Arithmetic Operator/Division.java
@@ -0,0 +1,8 @@
+class Division{
+      public static void main(String[]args){
+      int x=25;
+      int y=5;
+      int z=x/y;
+      System.out.println("Division of two number is:"+z);
+      }
+}
