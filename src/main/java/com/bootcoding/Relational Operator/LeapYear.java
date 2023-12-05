39be724511784978539bb3ab6ca59130c0b462c0
Tanmay <tanmaymvaidya@gmail.com>
2023-12-05 13:54:31 +0530

    Relational Operator program

---
+++ src/main/java/com/bootcoding/Relational Operator/LeapYear.java
@@ -0,0 +1,10 @@
+class LeapYear{
+      public static void main(String[]args){
+      int num=2024;
+      if(num%4==0){
+        System.out.println("It is leapyear");
+      }else{
+        System.out.println("It is not leapyear");
+      }
+      }
+}
