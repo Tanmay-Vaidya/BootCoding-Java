476bb053be3f1a855e2bc49e4082e699e138931b
Tanmay <tanmaymvaidya@gmail.com>
2023-12-05 13:21:56 +0530

    Increment Decrement Operator Program

---
+++ src/main/java/com/bootcoding/IncrementandDecrement Operator/PostPre.java
@@ -0,0 +1,11 @@
+class PostPre{
+      public static void main(String[]args){
+      int a=5;
+          a++;
+          System.out.println("Post increment is:"+a);
+
+          int b=9;
+          ++b;
+          System.out.println("Pre increment is:"+b);
+      }
+}
