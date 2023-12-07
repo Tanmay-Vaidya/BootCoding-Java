7047b630f9485bcf51bc29d03227b34575f9e60a
Tanmay <tanmaymvaidya@gmail.com>
2023-12-07 16:31:20 +0530

    program using if elseif Statement

---
+++ src/main/java/com/bootcoding/control flow/if statement/GradeCalculator.java
@@ -0,0 +1,18 @@
+class GradeCalculator{
+      public static void main(String[]args){
+      float a=30f;
+      
+      if(a>80){
+      System.out.println("Grade A");
+      }  
+      else if(a>60){
+      System.out.println("Grade B");
+      }
+      else if(a>40){
+      System.out.println("Grade C");
+      }
+      else if(a<35){
+      System.out.println("This is fail");
+      }
+      }
+}
