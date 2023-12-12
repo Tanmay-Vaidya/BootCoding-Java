b7688ec4216d0d1467c2750057e593057ffea06d
Tanmay <tanmaymvaidya@gmail.com>
2023-12-12 12:55:45 +0530

    Character on a new line

---
+++ src/main/java/com/bootcoding/loops/Character.java
@@ -0,0 +1,10 @@
+class Character{
+      public static void main(String[]args){
+      String name="Tanmay";
+      for(int i=0;i<name.length();i++)
+      {
+       char a=name.charAt(i);
+       System.out.println(a);
+      }
+      }
+}
