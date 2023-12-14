a7e8cac59c59c93a5d6c8e113ad278e50d4008f9
Tanmay <tanmaymvaidya@gmail.com>
2023-12-14 17:11:49 +0530

    Factorial Number using while loop

---
+++ main/java/com/bootcoding/while loop/FactorialNumber.java
@@ -0,0 +1,12 @@
+class FactorialNumber{
+      public static void main(String[]args){
+      int  j=1;
+      int fact=1;
+      
+      while(j<=5){
+           fact=fact*j;
+           j++;
+           }
+           System.out.println("Factorial is:"+fact);
+           }
+}
