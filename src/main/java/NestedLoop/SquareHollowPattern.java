38288c57a2c1372b67000973d419581987a5ed22
Tanmay <tanmaymvaidya@gmail.com>
2023-12-18 14:03:48 +0530

    program to prin square hollow pattern

---
+++ src/main/java/NestedLoop/SquareHollowPattern.java
@@ -0,0 +1,20 @@
+package NestedLoop;
+
+public class SquareHollowPattern {
+       public static void main (String[]args){
+           for(int j=1;j<=5;j++)
+           {
+               for(int z=1;z<=5;z++)
+               {
+                   if(j==1 || j==5 || z==1 || z==5)
+                   {
+                       System.out.print("*");
+                   }
+                   else{
+                       System.out.print(" ");
+                   }
+               }
+               System.out.print("\n");
+           }
+       }
+}
