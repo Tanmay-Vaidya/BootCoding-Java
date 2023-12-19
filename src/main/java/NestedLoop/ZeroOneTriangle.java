71d9a4e83dc5e4cc120fbfcc7b2f38035f9623e6
Tanmay <tanmaymvaidya@gmail.com>
2023-12-19 12:12:54 +0530

    Zero one triangle pattern in java using nested loop

--- src/main/java/NestedLoop/ZeroOneTriangle.java
+++ src/main/java/NestedLoop/ZeroOneTriangle.java
@@ -2,7 +2,7 @@ package NestedLoop;
 
 public class ZeroOneTriangle {
     public static void main(String[] args) {
-        for(int i=1;i<=5;i++)
+        for(int i=1;i<=6;i++)
         {
             for(int j=1;j<=i;j++)
             {
