783b6529d39f05eed7bf3aa359d67063d58e4299
Tanmay <tanmaymvaidya@gmail.com>
2023-12-19 16:16:24 +0530

    calculate factorial using scanner

---
+++ src/main/java/scanner_class/FactorialCalculator.java
@@ -0,0 +1,17 @@
+package scanner_class;
+import java.util.Scanner;
+public class FactorialCalculator {
+    public static void main(String[] args) {
+        System.out.println("Enter factorial number:");
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        int i=1;
+        int fact=1;
+        while(i<=num)
+        {
+            fact=fact*i;
+            i++;
+        }
+        System.out.println("Factorial is:"+fact);
+    }
+}
