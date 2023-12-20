dd087033d69ea1baf31f1aad1db67a90304db785
Tanmay <tanmaymvaidya@gmail.com>
2023-12-20 13:07:42 +0530

    sum of given number using command line argument

---
+++ src/main/java/command_line_arg/SumArg.java
@@ -0,0 +1,17 @@
+
+
+public class SumArg {
+    public static void main(String[] args) {
+        int num=Integer.parseInt(args[0]);
+        int sum=0;
+        int rem=0;
+
+        while(num>0)
+        {
+            rem=num%10;
+            sum=sum+rem;
+            num=num/10;
+        }
+        System.out.println("The given integer sum is:"+sum);
+    }
+}
