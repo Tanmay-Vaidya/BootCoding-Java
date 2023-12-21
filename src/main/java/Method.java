29426ab8399fa536810047b9ba02df809bef5f4f
Tanmay <tanmaymvaidya@gmail.com>
2023-12-21 18:26:00 +0530

    method example in java

---
+++ src/main/java/Method.java
@@ -0,0 +1,19 @@
+public class Method {
+    public static void sayHi() {
+        System.out.println("Hello roman!");
+        System.out.println("What going on!");
+    }
+
+    public static void sayBy() {
+        System.out.println("Ok roman");
+        System.out.println("I will see you later");
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Message 1");
+        sayHi();
+        System.out.println("Message 2");
+        sayBy();
+    }
+}
