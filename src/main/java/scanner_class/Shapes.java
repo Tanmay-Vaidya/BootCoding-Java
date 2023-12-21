d46b33606ff5cb7135047dfc22ccbb5dd7914f78
Tanmay <tanmaymvaidya@gmail.com>
2023-12-21 18:07:43 +0530

    find area of circle,square,& triangle using scanner class

---
+++ src/main/java/scanner_class/Shapes.java
@@ -0,0 +1,53 @@
+package scanner_class;
+import java.util.Scanner;
+public class Shapes {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        char p;
+        float r, ar;
+        int s, b, h;
+        System.out.println("Enter c for circle" +
+                "Enter s for square" +
+                " Enter t for triangle:");
+        p = sc.next().charAt(0);
+
+        switch (p) {
+            case 'c':
+                System.out.println("Enter radius");
+                r = sc.nextFloat();
+                ar = 22 / 7 * r * r;
+                System.out.println("Area of circle is" + ar);
+                break;
+
+            case 's':
+                System.out.println("Enter side");
+                s = sc.nextInt();
+                ar = s * s;
+                System.out.println("Area of square is" + ar);
+                break;
+
+            case 't':
+                System.out.println("Enter base or height");
+                b = sc.nextInt();
+                h = sc.nextInt();
+                ar = 1 / 2 * b * h;
+                System.out.println("Area of triangle is" + ar);
+                break;
+
+            default: {
+                System.out.println("Wrong input");
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
