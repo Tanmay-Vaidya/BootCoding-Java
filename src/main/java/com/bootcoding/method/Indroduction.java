faa8fcd1100cfa42bb26fe3f807ccaf94427f390
Tanmay <tanmaymvaidya@gmail.com>
2023-12-22 16:18:51 +0530

    using multiple method indrotuction

---
+++ src/main/java/com/bootcoding/method/Indroduction.java
@@ -0,0 +1,42 @@
+package com.bootcoding.method;
+
+public class Indroduction {
+    public void method1(){
+        System.out.println("My name is tanmay vaidya");
+    }
+    public void method2(){
+        System.out.println("I am from maharashtra,nagpur");
+    }
+
+    public void method3(){
+        System.out.println("I hava completed b.com computer application in rashtrasant tukadoji maharaj university");
+    }
+
+    public void method4(){
+        System.out.println("I am currently studying in master of computer application in G.H.Raisoni University Amravati");
+    }
+
+    public void method5(){
+        System.out.println("I am 22 year old");
+    }
+
+    public void method6(){
+        System.out.println("I am hardworking");
+    }
+
+    public void method7(){
+        System.out.println("My short term goal is getting a job in reputed company");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Introduction:");
+        Indroduction obj=new Indroduction();
+        obj.method1();
+        obj.method2();
+        obj.method3();
+        obj.method4();
+        obj.method5();
+        obj.method6();
+        obj.method7();
+    }
+}
