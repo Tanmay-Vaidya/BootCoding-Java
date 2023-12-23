e586a1eb7832719d2e0a5a6964d1b12dc05dd73d
Tanmay <tanmaymvaidya@gmail.com>
2023-12-23 14:11:10 +0530

    armstrong number using method

---
+++ src/main/java/com/bootcoding/method/ArmstrongUsingMethod.java
@@ -0,0 +1,27 @@
+package com.bootcoding.method;
+import java.util.Scanner;
+public class ArmstrongUsingMethod {
+    public static int armstrong(int n){
+        int arm=0, a , b;
+            b=n;
+        while (n > 0) {
+            a = n % 10;
+            arm = (a * a * a *a) + arm;
+            n = n / 10;
+        }
+            if (b==arm) {
+                System.out.println("These is Armstrong Number:");
+            } else {
+                System.out.println("These is Not Armstrong Number:");
+            }
+        return arm;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter any number:");
+        int num=sc.nextInt();
+
+        System.out.println(armstrong(num));
+    }
+}
