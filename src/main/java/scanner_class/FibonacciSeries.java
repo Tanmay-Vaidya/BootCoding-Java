18d4d57004e45ef469656bebde5a7bd3d274d01e
Tanmay <tanmaymvaidya@gmail.com>
2023-12-23 13:15:59 +0530

    fibonacci series using scanner class

---
+++ src/main/java/scanner_class/FibonacciSeries.java
@@ -0,0 +1,20 @@
+package scanner_class;
+import java.util.Scanner;
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        int a=0,b=1,c;
+
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter term:");
+        int num=sc.nextInt();
+
+        for (int k=0 ; k<=num ; k++ ){
+            System.out.print(a+" ");
+            c=a+b;
+            a=b;
+            b=c;
+
+        }
+
+    }
+}
