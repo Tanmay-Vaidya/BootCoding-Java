79b80f6ea2e581853872589921570c59e33bee98
Tanmay <tanmaymvaidya@gmail.com>
2023-12-27 15:33:08 +0530

    floating point  array element program

---
+++ src/main/java/com/bootcoding/array/CharacterArray.java
@@ -0,0 +1,13 @@
+package com.bootcoding.array;
+
+public class CharacterArray {
+    public static void main(String[] args) {
+        char arr[]={'a','b','c','d','e'};
+
+        for (char i=0;i< args.length;i++)
+        {
+            System.out.println(arr[i]);
+            System.out.println(arr.length);
+        }
+    }
+}
