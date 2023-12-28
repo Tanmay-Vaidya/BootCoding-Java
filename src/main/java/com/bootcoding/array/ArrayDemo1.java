be8700930234f7f1535e8668d6817ddaec29210a
Tanmay <tanmaymvaidya@gmail.com>
2023-12-28 14:17:49 +0530

    array example in datatypes

---
+++ src/main/java/com/bootcoding/array/ArrayDemo1.java
@@ -0,0 +1,17 @@
+package com.bootcoding.array;
+
+public class ArrayDemo1 {
+    public static void main(String[] args) {
+        int arr[]=new int[10];
+        System.out.println(arr.length+" "+arr[0]);
+
+        boolean barr[]=new boolean[5];
+        System.out.println(barr.length+" "+barr[0]);
+
+        String sarr[]=new String[6];
+        System.out.println(sarr.length+" "+sarr[0]);
+
+        char carr[]=new char['A'];
+        System.out.println(carr.length+" "+carr[0]);
+    }
+}
